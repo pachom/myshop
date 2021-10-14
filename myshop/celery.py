b147d93b2c2dca655c3fd464c2e1de19231ebb61
Francisco <pachom@gmail.com>
2021-10-14 05:18:40 -0500

    Initial myshop project without payment.

---
+++ myshop/celery.py
@@ -0,0 +1,15 @@
+import os
+from celery import Celery
+from django.conf import settings
+
+# set the default Django settings module for the 'celery' program.
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myshop.settings')
+
+app = Celery('myshop')
+
+app.config_from_object('django.conf:settings', namespace='CELERY')
+app.autodiscover_tasks()
+
+@app.task(bind=True)
+def debug_task(self):
+    print(f'Request: {self.request!r}')
